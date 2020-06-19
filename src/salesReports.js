a1c4ae11d426561574c9acf51cdb9fea59af215d
Gosha <djabarov.george@gmail.com>
2020-06-19 21:01:14 +0300

    refactor giveByDate

--- src/salesReports.js
+++ src/salesReports.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 const { Connection, Request } = require("tedious");
 const salesReport = require("./salesReport")
+const db=require('./db/index.js');
 // Create connection to database
 const config = {
     authentication: {
@@ -110,89 +111,106 @@ class salesReports
         connection.execSql(request);
       }
     }); 
-
     }//end of getSalesReport function
+
+
+
+    static giveByDateB(fromDate, toDate,callback){
+      db.request(`SELECT c.cartID, c.PID, p.Pname, c.productAmount, c.totalPrice, c.cDate, c.email, u.fullName
+        FROM [dbo].[Cart] AS c , [dbo].[Product] AS p, [dbo].[Usernames] AS u
+        WHERE c.PID = p.PID AND c.email = u.email AND c.cDate >= '`+fromDate+`' AND c.cDate <= '`+toDate+`'`,(products)=>{
+      var salesReports =[];
+      products.forEach(product => {
+        var salesReport1 = new salesReport(product.cartID, product.PID, product.Pname, product.Pamount, product.totalPrice, product.cDate, product.customerName, product.customerEmail);
+        salesReports.push(salesReport1); 
+      });
+      return callback(salesReports);
+      });
+    };
+
+
     static giveByDate(fromDate, toDate,callback){
       console.log(fromDate);
       console.log("in Date filter class");
-      let connection = new Connection(config);
-    connection.on("connect", err => {
-      if (err) {
-        console.error(err.message);
-      } else {
-      const request = new Request( 
-          `SELECT c.cartID, c.PID, p.Pname, c.productAmount, c.totalPrice, c.cDate, c.email, u.fullName
-          FROM [dbo].[Cart] AS c , [dbo].[Product] AS p, [dbo].[Usernames] AS u
-          WHERE c.PID = p.PID AND c.email = u.email AND c.cDate >= '`+fromDate+`' AND c.cDate <= '`+toDate+`'`,
-        (err, rowCount,rows) => {
-          if (err) {
-            console.error(err.message);
-            return callback(false);
-          } 
-          else {
-            console.log(`${rowCount} row(s) returned`);
-            connection.close();
-            var salesReports =[];
-            rows.forEach(element => {
-              var cartID, PID, Pname, Pamount, totalPrice, cDate, customerName, customerEmail;
-              element.forEach(column =>{
-                switch(column.metadata.colName)
-                {
-                  case 'cartID': 
-                  {
-                      cartID = column.value;
-                    break;
-                  }
-                  case 'PID': 
-                  {
-                      PID = column.value;
-                    break;
-                  }
-                  case 'Pname': 
-                  {
-                      Pname = column.value;
-                    break;
-                  }
-                  case 'productAmount': 
-                  {
-                      Pamount = column.value;
-                    break;
-                  }
-                  case 'totalPrice': 
-                  {
-                      totalPrice = column.value;
-                    break;
-                  }
-                  case 'cDate': 
-                  {
-                      cDate = column.value
-                      cDate.setMonth(column.value.getMonth()+1) ;
-                    break;
-                  }
-                  case 'fullName': 
-                  {
-                      customerName = column.value ;
-                    break;
-                  }                    
-                  case 'email': 
-                  {
-                      customerEmail = column.value;
-                    break;
-                  }                   
-                }// end of switch 
-              });
-              // console.log(cartID, PID, Pname, Pamount, totalPrice, cDate, customerName, customerEmail);
+      this.giveByDateB(fromDate, toDate,callback);
+  //     let connection = new Connection(config);
+  //   connection.on("connect", err => {
+  //     if (err) {
+  //       console.error(err.message);
+  //     } else {
+  //     const request = new Request( 
+  //         `SELECT c.cartID, c.PID, p.Pname, c.productAmount, c.totalPrice, c.cDate, c.email, u.fullName
+  //         FROM [dbo].[Cart] AS c , [dbo].[Product] AS p, [dbo].[Usernames] AS u
+  //         WHERE c.PID = p.PID AND c.email = u.email AND c.cDate >= '`+fromDate+`' AND c.cDate <= '`+toDate+`'`,
+  //       (err, rowCount,rows) => {
+  //         if (err) {
+  //           console.error(err.message);
+  //           return callback(false);
+  //         } 
+  //         else {
+  //           console.log(`${rowCount} row(s) returned`);
+  //           connection.close();
+  //           var salesReports =[];
+  //           rows.forEach(element => {
+  //             var cartID, PID, Pname, Pamount, totalPrice, cDate, customerName, customerEmail;
+  //             element.forEach(column =>{
+  //               switch(column.metadata.colName)
+  //               {
+  //                 case 'cartID': 
+  //                 {
+  //                     cartID = column.value;
+  //                   break;
+  //                 }
+  //                 case 'PID': 
+  //                 {
+  //                     PID = column.value;
+  //                   break;
+  //                 }
+  //                 case 'Pname': 
+  //                 {
+  //                     Pname = column.value;
+  //                   break;
+  //                 }
+  //                 case 'productAmount': 
+  //                 {
+  //                     Pamount = column.value;
+  //                   break;
+  //                 }
+  //                 case 'totalPrice': 
+  //                 {
+  //                     totalPrice = column.value;
+  //                   break;
+  //                 }
+  //                 case 'cDate': 
+  //                 {
+  //                     cDate = column.value
+  //                     cDate.setMonth(column.value.getMonth()+1) ;
+  //                   break;
+  //                 }
+  //                 case 'fullName': 
+  //                 {
+  //                     customerName = column.value ;
+  //                   break;
+  //                 }                    
+  //                 case 'email': 
+  //                 {
+  //                     customerEmail = column.value;
+  //                   break;
+  //                 }                   
+  //               }// end of switch 
+  //             });
+  //             // console.log(cartID, PID, Pname, Pamount, totalPrice, cDate, customerName, customerEmail);
 
-                var salesReport1 = new salesReport(cartID, PID, Pname, Pamount, totalPrice, cDate, customerName, customerEmail);
-                salesReports.push(salesReport1); 
-            });
-            return callback(salesReports);
-          }
-        }
-      );
-      connection.execSql(request);
-    }
-  }); 
+  //               var salesReport1 = new salesReport(cartID, PID, Pname, Pamount, totalPrice, cDate, customerName, customerEmail);
+  //               salesReports.push(salesReport1); 
+  //           });
+  //           return callback(salesReports);
+  //         }
+  //       }
+  //     );
+  //     connection.execSql(request);
+  //   }
+  // }); 
 
   }//end of filterbydate function
   static filterByEmail(emailFilter,callback){
